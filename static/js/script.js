7af3f846302953d19c832c6f96107e0cb78b2f4d
andrew2635 <andrewleone2007@gmail.com>
2025-02-25 09:39:13 +0100

    final commit

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+let url = "http://randomuser.me/api/"
+let globalPerson
+
+fetch(url).then(
+    (response) => response.json()
+).then(
+    function(data){
+        globalPerson = data.results[0]
+        console.log(globalPerson)
+        document.getElementById('name').innerHTML = globalPerson.name.first+ " " + globalPerson.name.last;
+        document.getElementById('email').innerHTML = globalPerson.email;
+        document.getElementById('location').innerHTML = globalPerson.location.city+ " " + globalPerson.location.country;
+        document.getElementById('phone').innerHTML = globalPerson.phone;
+        document.getElementById('picture').src = globalPerson.picture.large;
+    }
+)
